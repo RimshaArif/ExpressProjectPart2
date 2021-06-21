d810be511ea0c5ba875f321f5e535b5994052b73
RimshaArif <rimshaarif723@gmail.com>
2021-06-22 02:04:58 +0500

    commit

--- myapplication2/routes/users.js
+++ myapplication2/routes/users.js
@@ -2,11 +2,8 @@ var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get(
-  "https://web-assignment3-heroku.herokuapp.com/",
-  function (req, res, next) {
-    res.send("respond with a resource");
-  }
-);
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
 
 module.exports = router;
